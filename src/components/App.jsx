1e09e95f1a85e708dc4d9dc026f7115f14cfdd67
leoportogtr86 <leoporto_studio@hotmail.com>
2020-10-25 14:31:44 -0300

    botao de texto

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Botao from './Botao'
+
+
+
+export default () => {
+
+    return (
+
+        <div>
+            <h1>App</h1>
+
+            <Botao />
+        </div>
+    )
+}
---
+++ src/components/Botao.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+
+
+export default props => {
+
+    return (
+
+        <div>
+
+            <Button color="primary">Botão de Texto</Button>
+
+        </div>
+    )
+}
