953b1c110fbf37d14fb618da09e8d868195340f7
leoportogtr86 <leoporto_studio@hotmail.com>
2020-10-25 16:47:55 -0300

    material-ui/icons

--- src/components/Botao.jsx
+++ src/components/Botao.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import { DeleteIcon } from '@material-ui/icons/Delete'
 
 //https://material-ui.com/pt/components/buttons/
 
